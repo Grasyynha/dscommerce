1251999d8311aa8665e34e3f3f6dd0a1aaf9405a
Grasiela Texeira <grasy.mst@gmail.com>
2023-06-02 09:54:09 -0300

    Criação da pasta Category

---
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")  // Customizar o nome da tabela no Banco de dados
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private  String name;
+
+    @ManyToMany(mappedBy = "categories")
+
+    private Set<Product> products = new HashSet<>();
+
+    public Category(){
+
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
+}
